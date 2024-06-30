718324b89363113a27643ddf007fd93663c87b33
perwriters <perwriters@gmail.com>
2024-06-30 19:27:52 +0300

    basic operations 1

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,3 +1,4 @@
+#  specific values for the two variables
 number1 = 10
 number2 = 5
 
@@ -6,7 +7,7 @@ addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 
-# Print the results of these operations in the specified format
-print(f"[addition] of {number1} and {number2} is {addition}")
-print(f"[subtraction] of {number1} and {number2} is {subtraction}")
-print(f"[multiplication] of {number1} and {number2} is {multiplication}")
+# Print the results of these operations
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
